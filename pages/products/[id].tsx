a1ebabdb4621548d4cba9188e3fc79a88766b89e
abderrahmaneosnai <abderrahmaneosmani2@gmail.com>
2022-02-25 17:53:15 -0500

    update server

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -11,7 +11,7 @@ function getProductById({ product }: any) {
 export const getServerSideProps = async (context: any) => {
   const id = context.params.id;
   const res = await fetch(
-    `https://ecommerce-front-end-1cco9v8el-abderrahmaneosmani.vercel.app/api/products/${id}`
+    `https://ecommerce-front-end-88to5asfu-abderrahmaneosmani.vercel.app/api/products/${id}`
   );
   const product = await res.json();
 
